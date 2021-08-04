d858d7064d7bf79132b957f73a85dadda3e62a8e
Sean Marquez <capsulecorplab@gmail.com>
2021-08-03 22:21:48 -0600

    remove serve.sh & update README
    Removing references to running a local webserver, as current workflow doesn't include a local webserver

--- serve.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs && hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender --destination public'
