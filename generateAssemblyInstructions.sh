ade53b1e1d282a73e363e90834346917730a591c
Sean Marquez <capsulecorplab@gmail.com>
2023-07-31 21:43:19 +0000

    create variables for setting path to node binaries

--- generateAssemblyInstructions.sh
+++ generateAssemblyInstructions.sh
@@ -2,6 +2,9 @@
 
 #### set environment variable for project root ####
 project_root=$PWD
+node_modules_path=$(readlink -f node_modules)
+dof_helpers_path=$(readlink -f dof-helpers)
+asciidoctor_path=$node_modules_path/.bin/asciidoctor
 
 # Make dist/ directory, if none exists
 if [ ! -r ./dist ]; then
@@ -30,11 +33,11 @@ if [ ! -r ./node_modules ]; then
 fi
 
 # copy node_modules into dist/
-cp -r ./node_modules ./dist
+cp -r $node_modules_path ./dist
 
 #### generate dist/component.yaml & symlink to architecture/ directory ####
 clitool="node"
-cmdargs="dof-helpers/parseComponent.js"
+cmdargs="$dof_helpers_path/parseComponent.js"
 cmd="$clitool $cmdargs"
 workdir=$project_root
 podmancmd="podman run --rm --volume $workdir:/srv -w /srv docker.io/node $cmdargs"
@@ -53,7 +56,7 @@ fi
 
 #### generate assembly instructions as asciidoc ####
 clitool="node"
-cmdargs="dof-helpers/generateAssemblyInstructions.js"
+cmdargs="$dof_helpers_path/generateAssemblyInstructions.js"
 cmd="$clitool $cmdargs"
 workdir=$project_root
 podmancmd="podman run --rm --volume $workdir:/srv -w /srv docker.io/node $cmdargs"
@@ -71,7 +74,7 @@ else
 fi
 
 #### generate assembly instructions as html ####
-clitool="./node_modules/.bin/asciidoctor"
+clitool=$asciidoctor_path
 cmdargs="assemblyInstructions.adoc -o assemblyInstructions.html"
 cmd="$clitool $cmdargs"
 workdir=$project_root/dist
@@ -90,7 +93,7 @@ else
 fi
 
 #### generate assembly instructions as pdf ####
-clitool="./node_modules/.bin/asciidoctor"
+clitool=$asciidoctor_path
 cmdargs="assemblyInstructions.adoc -o assemblyInstructions.pdf -r asciidoctor-pdf -b pdf"
 cmd="$clitool $cmdargs"
 workdir=$project_root/dist
